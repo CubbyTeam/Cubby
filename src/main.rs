c2d7d79c83389757ed52bbd444a7ceaf86d6d20c
Chris Ohk <utilforever@gmail.com>
2022-03-01 23:57:17 +0900

    feat(ecs): Add code to test crate 'legion'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use legion::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Velocity {
+    dx: f32,
+    dy: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut world = World::default();
+
+    let _: &[Entity] = world.extend(vec![
+        (Position { x: 0.0, y: 0.0 }, Velocity { dx: 0.0, dy: 0.0 }),
+        (Position { x: 1.0, y: 1.0 }, Velocity { dx: 0.0, dy: 0.0 }),
+        (Position { x: 2.0, y: 2.0 }, Velocity { dx: 0.0, dy: 0.0 }),
+    ]);
+
+    let mut query = <&Position>::query();
+
+    for position in query.iter(&world) {
+        println!("{:?}", position);
+    }
 }
